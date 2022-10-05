ae96d8644809bc20a9cb0c276f4a423a1c4a715d
thegreenearth <juniorprog.com@pccpl.ac.th>
2022-10-05 19:49:07 +0700

    add if

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
 print("Hello Python from Local")
 print("Hello Python from Github.com")
-for i in range(100):
+for i in range(1,100):
     if i % 2 == 0:
-        print(i)
+        print(i,end=" ")
+    else:
+        print(i,end=" ")
